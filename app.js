258473d10058938ec0f55ce4098ca5395b58ba4b
AnnaYang <46180586+AnnaYangKorea@users.noreply.github.com>
2020-01-17 13:02:49 +0900

    20200117

---
+++ app.js
@@ -0,0 +1,40 @@
+const canvas = document.getElementById("jsCanvas");
+
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function stopPainting() {
+  painting = false;
+}
+function startPainting() {
+  painting = true;
+}
+
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+  }
+  //console.log(x, y);
+}
+
+function onMouseUp(event) {
+  //console.log(event);
+  stopPainting();
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
